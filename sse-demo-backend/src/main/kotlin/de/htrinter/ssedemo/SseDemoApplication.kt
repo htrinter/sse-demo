a70fd353ab7fe1aceebc3374a741754268d8163d
htrinter <htrinter@posteo.de>
2019-04-18 20:15:35 +0200

    Add service for mocking of job executions

--- sse-demo-backend/src/main/kotlin/de/htrinter/ssedemo/SseDemoApplication.kt
+++ sse-demo-backend/src/main/kotlin/de/htrinter/ssedemo/SseDemoApplication.kt
@@ -2,8 +2,10 @@ package de.htrinter.ssedemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class SseDemoApplication
 
 fun main(args: Array<String>) {
---
+++ sse-demo-backend/src/main/kotlin/de/htrinter/ssedemo/service/MockJobRunnerService.kt
@@ -0,0 +1,51 @@
+package de.htrinter.ssedemo.service
+
+import de.htrinter.ssedemo.domain.JobData
+import de.htrinter.ssedemo.domain.JobExitCode
+import de.htrinter.ssedemo.domain.JobStatus
+import org.slf4j.LoggerFactory
+import org.springframework.messaging.SubscribableChannel
+import org.springframework.messaging.support.GenericMessage
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import java.util.*
+import kotlin.collections.ArrayList
+
+@Service
+class MockJobRunnerService(private var jobDataChannel: SubscribableChannel) {
+
+  private val LOG = LoggerFactory.getLogger(MockJobRunnerService::class.java)
+  private val MAX_RUNNING_JOBS = 30
+
+  private val random = Random()
+  private val runningJobList = Collections.synchronizedList(ArrayList<JobData>())
+
+  @Scheduled(fixedDelay = 300)
+  fun updateJobs() {
+    // Schedule job if we are running out
+    if (runningJobList.size < MAX_RUNNING_JOBS) {
+      val newJobData = JobData(UUID.randomUUID().toString(), "A long running job");
+      runningJobList.add(newJobData)
+      LOG.debug("Job added, id=%s".format(newJobData.id))
+    }
+
+    // Update one of the existing jobs
+    val jobIndex = random.nextInt(runningJobList.size)
+    val jobData = runningJobList.get(jobIndex)
+
+    val completionInc = random.nextInt(30)
+    if (jobData.completionPercentage + completionInc > 100) {
+      jobData.completionPercentage = 100
+      jobData.status = JobStatus.FINISHED
+      jobData.exitCode = JobExitCode.SUCCESS
+      LOG.debug("Job completed, id=%s, exitCode=%s".format(jobData.id, jobData.exitCode))
+    } else {
+      jobData.completionPercentage += completionInc
+      jobData.status = JobStatus.IN_PROGRESS
+      LOG.debug("Job progress incremented, id=%s, completionPercentage=%s".format(jobData.id, jobData.completionPercentage))
+    }
+
+    jobDataChannel.send(GenericMessage<JobData>(jobData))
+  }
+
+}
